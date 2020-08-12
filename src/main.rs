c8115a7462e57d3b5c495e3c25421ca5c6cefe06
Ido Barkan <ido@appsflyer.com>
2020-08-12 16:32:38 +0300

    walking skeleton

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use std::net::TcpListener;
+use zero2prod::run;
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
+    // Bubble up the io::Error if we failed to bind the address
+    // Otherwise call .await on our Server
+    run(listener)?.await
+}
