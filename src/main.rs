d9eec2ebbc83444fabf57a8258110a70c367a23e
Ido Barkan <ido@appsflyer.com>
2020-09-27 14:01:49 +0300

    end of chapter 3.5

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
+use sqlx::PgPool;
 use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
-    // Bubble up the io::Error if we failed to bind the address
-    // Otherwise call .await on our Server
-    run(listener)?.await
+    // Panic if we can't read configuration
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection)?.await
 }
