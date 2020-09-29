47b5693d8649f0c54b4bd86171bac4d0f90bde17
Ido Barkan <ido@appsflyer.com>
2020-09-29 10:08:21 +0300

    wip 5.10 dows not compile yet

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
+use anyhow::Context;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[actix_rt::main]
-async fn main() -> std::io::Result<()> {
-    // Panic if we can't read configuration
+async fn main() -> Result<(), anyhow::Error> {
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection = PgPool::connect(&configuration.database.connection_string())
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
-        .expect("Failed to connect to Postgres.");
+        .map_err(anyhow::Error::from)
+        .with_context(|| "Failed to connect to Postgres.")?;
+
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener, connection)?.await
+    run(listener, connection_pool)?.await
 }
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,8 @@
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
+use tracing;
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -10,24 +12,42 @@ pub struct FormData {
 }
 
 pub async fn subscribe(
-    form: web::Form<FormData>,
-    connection: web::Data<PgPool>,
+    payload: web::Form<FormData>,
+    pool: web::Data<PgPool>,
 ) -> Result<HttpResponse, HttpResponse> {
+    let request_id = Uuid::new_v4();
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        email = %payload.email,
+        name = %payload.name
+    );
+    let _request_span_guard = request_span.enter();
+    let query_span = tracing::info_span!("Saving new subscriber details in the database");
     sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
         VALUES ($1, $2, $3, $4)
         "#,
         Uuid::new_v4(),
-        form.email,
-        form.name,
+        payload.email,
+        payload.name,
         Utc::now()
     )
-    .execute(connection.get_ref())
+    .execute(pool.as_ref())
+    .instrument(query_span)
     .await
     .map_err(|e| {
-        eprintln!("Failed to execute query: {}", e);
+        tracing::error!(
+            "request_id {} - Failed to execute query: {:?}",
+            request_id,
+            e
+        );
         HttpResponse::InternalServerError().finish()
     })?;
+    tracing::info!(
+        "request_id {} - New subscriber details have been saved",
+        request_id
+    );
     Ok(HttpResponse::Ok().finish())
 }
